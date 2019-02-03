10f249255fd259edc798b0b4322978460f207dad
Veronica Bustabad <vbustabad@gmail.com>
2019-02-03 02:47:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 function bfs(rootNode, vertices, edges){
+   let rootNode = vertices[0];
+   let queue = [];
+   queue.push(rootNode);
 
+   while (queue.length !== 0) {
+       let firstNode = queue.shift();
+       let adjacentVertices = findAdjacentNodes(firstNode);
+       for vertex in adjacentVertices {
+           markDistanceAndPredecessor(vertex);
+           queue.push(vertex);
+       }
+   }
+}
+
+function findAdjacentNodes() {
+
+}
+
+function markDistanceAndPredecessor(vertex) {
+  if vertex
 }
